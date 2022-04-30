d8a4923898973ae735df81330e9af3cb6af78bde
João Victor Duarte <joaodicu387@gmail.com>
2022-04-30 18:02:31 -0300

    Namespaces

--- Slice It/Assets/Scripts/Player/KnifeCollider.cs
+++ Slice It/Assets/Scripts/Player/KnifeCollider.cs
@@ -3,46 +3,49 @@ using UnityEngine;
 
 using Desafio.Slices;
 
-public sealed class KnifeCollider : MonoBehaviour
+namespace Desafio.Knife
 {
-    [SerializeField] private Rigidbody rb;
-    [SerializeField] private Collider collider;
-    [SerializeField] private float delayToStartDetectorColliderInSeconds = 1.0f;
-
-    public void EnabledColliderInSeconds()
+    public sealed class KnifeCollider : MonoBehaviour
     {
-        StartCoroutine(EnableColliderCoroutine());
-    }
+        [SerializeField] private Rigidbody rb;
+        [SerializeField] private Collider collider;
+        [SerializeField] private float delayToStartDetectorColliderInSeconds = 1.0f;
 
-    private IEnumerator EnableColliderCoroutine()
-    {
-        yield return new WaitForSeconds(delayToStartDetectorColliderInSeconds);
-        collider.enabled = true;
-    }
-
-    private void SleepKnife()
-    {
-        collider.enabled = false;
-        rb.isKinematic = true;
-    }
-
-    private void Slice(Slice slice)
-    {
-        slice.Execute();
-    }
-
-    private void OnTriggerEnter(Collider collider)
-    {
-        Ground isGround = collider.GetComponent<Ground>();
-        if(isGround != null)
+        public void EnabledColliderInSeconds()
         {
-            SleepKnife();
+            StartCoroutine(EnableColliderCoroutine());
         }
 
-        Slice canSlice = collider.GetComponent<Slice>();
-        if(canSlice != null)
+        private IEnumerator EnableColliderCoroutine()
         {
-            Slice(canSlice);
+            yield return new WaitForSeconds(delayToStartDetectorColliderInSeconds);
+            collider.enabled = true;
+        }
+
+        private void SleepKnife()
+        {
+            collider.enabled = false;
+            rb.isKinematic = true;
+        }
+
+        private void Slice(Slice slice)
+        {
+            slice.Execute();
+        }
+
+        private void OnTriggerEnter(Collider collider)
+        {
+            Ground isGround = collider.GetComponent<Ground>();
+            if(isGround != null)
+            {
+                SleepKnife();
+            }
+
+            Slice canSlice = collider.GetComponent<Slice>();
+            if(canSlice != null)
+            {
+                Slice(canSlice);
+            }
         }
     }
 }
--- Slice It/Assets/Scripts/Player/KnifeController.cs
+++ Slice It/Assets/Scripts/Player/KnifeController.cs
@@ -1,67 +1,70 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-public sealed class KnifeController : MonoBehaviour
+namespace Desafio.Knife
 {
-    [SerializeField] private Rigidbody rb;
-    [SerializeField] private float forceToUpShoot = 200;
-    [SerializeField] private float torqueShoot = 80;
-    private bool canShoot = false;
-
-    [Range(30, 85)]
-    [SerializeField] private float degressToShoot = 85;
-    private Vector3 directionToShoot;
-
-    [SerializeField] private UnityEvent onKnifeAttacked;
-
-    private void Start()
+    public sealed class KnifeController : MonoBehaviour
     {
-        CalculateDirectionToForce();
-    }
+        [SerializeField] private Rigidbody rb;
+        [SerializeField] private float forceToUpShoot = 200;
+        [SerializeField] private float torqueShoot = 80;
+        private bool canShoot = false;
 
-    private void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Space))
+        [Range(30, 85)]
+        [SerializeField] private float degressToShoot = 85;
+        private Vector3 directionToShoot;
+
+        [SerializeField] private UnityEvent onKnifeAttacked;
+
+        private void Start()
         {
-            canShoot = true;
-            onKnifeAttacked?.Invoke();
+            CalculateDirectionToForce();
         }
-    }
 
-    private void FixedUpdate()
-    {
-        if(canShoot)
+        private void Update()
         {
-            Shoot();
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                canShoot = true;
+                onKnifeAttacked?.Invoke();
+            }
         }
-    }
 
-    private void Shoot()
-    {
-        rb.isKinematic = false;
-        canShoot = false;
-        rb.AddForce(directionToShoot * forceToUpShoot);
-        rb.AddTorque(Vector3.forward * torqueShoot);
-    }
-
-    private void CalculateDirectionToForce()
-    {
-        float x = Mathf.Cos(degressToShoot * Mathf.Deg2Rad);
-        float y = Mathf.Sin(degressToShoot * Mathf.Deg2Rad);
-        directionToShoot = new Vector3(x, y, 0);
-    }
-
-    private void GameOver()
-    {
-        Debug.Log("Game Over");
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        GameOverCollider isGameOver = collision.gameObject.GetComponent<GameOverCollider>();
-        if (isGameOver != null)
+        private void FixedUpdate()
         {
-            GameOver();
+            if(canShoot)
+            {
+                Shoot();
+            }
+        }
+
+        private void Shoot()
+        {
+            rb.isKinematic = false;
+            canShoot = false;
+            rb.AddForce(directionToShoot * forceToUpShoot);
+            rb.AddTorque(Vector3.forward * torqueShoot);
+        }
+
+        private void CalculateDirectionToForce()
+        {
+            float x = Mathf.Cos(degressToShoot * Mathf.Deg2Rad);
+            float y = Mathf.Sin(degressToShoot * Mathf.Deg2Rad);
+            directionToShoot = new Vector3(x, y, 0);
+        }
+
+        private void GameOver()
+        {
+            Debug.Log("Game Over");
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            GameOverCollider isGameOver = collision.gameObject.GetComponent<GameOverCollider>();
+            if (isGameOver != null)
+            {
+                GameOver();
+            }
         }
     }
 }
