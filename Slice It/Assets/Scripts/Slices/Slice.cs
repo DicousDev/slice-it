bc91a4e3b449274aea758580b4077ba83a325475
João Victor Duarte <joaodicu387@gmail.com>
2022-05-01 03:54:38 -0300

    Particulas ao fazer cortes

--- Slice It/Assets/Scripts/Slices/Slice.cs
+++ Slice It/Assets/Scripts/Slices/Slice.cs
@@ -8,12 +8,20 @@ namespace SliceIt.Slices
         [SerializeField] private Collider colliderToDisabled;
         [SerializeField] private GameObject toDisabled;
         [SerializeField] private GameObject toEnabled;
+        [SerializeField] private GameObject particle;
 
         public void Execute()
         {
             colliderToDisabled.enabled = false;
             toDisabled.SetActive(false);
             toEnabled.SetActive(true);
+            InstantiateParticles();
+        }
+
+        private void InstantiateParticles()
+        {
+            GameObject particle = Instantiate(this.particle, transform.position, Quaternion.identity);
+            Destroy(particle, 1f);
         }
     }
 }
